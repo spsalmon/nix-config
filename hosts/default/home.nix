66c31ac79afe7ccd2e8bbb3d9cd0ac2b3c884321
spsalmon <psalmonsacha@gmail.com>
2024-03-10 13:03:04 +0100

    installing alacritty

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -37,6 +37,22 @@
 
   wayland.windowManager.hyprland.enable = true;
 
+    wayland.windowManager.hyprland.settings = {
+    decoration = {
+      shadow_offset = "0 5";
+      "col.shadow" = "rgba(00000099)";
+    };
+
+    "$mod" = "SUPER";
+
+    bindm = [
+      # mouse movements
+      "$mod, mouse:272, movewindow"
+      "$mod, mouse:273, resizewindow"
+      "$mod ALT, mouse:272, resizewindow"
+    ];
+  };
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
