4085e58601d213a86aa417bab8773c2e7b0ebbb5
spsalmon <psalmonsacha@gmail.com>
2024-03-30 18:55:03 +0100

    tons of keybinds for hyprland

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -30,19 +30,59 @@
 
     "$terminal" = "alacritty";
 
-    "$mod" = "SUPER";
+    "$mainMod" = "SUPER";
 
     bindm = [
-      # mouse movements
-      "$mod, mouse:272, movewindow"
-      "$mod, mouse:273, resizewindow"
-      "$mod ALT, mouse:272, resizewindow"
+      # Scroll through existing workspaces with mainMod + scroll
+      "$mainMod, mouse_down, workspace, e+1"
+      "$mainMod, mouse_up, workspace, e-1"
+
+      # Move/resize windows with mainMod + LMB/RMB and dragging
+      "bindm = $mainMod, mouse:272, movewindow"
+      "bindm = $mainMod, mouse:273, resizewindow"
     ];
     bind=[
-      # launch alacritty
-      "$mod, A, exec, alacritty"
-      "$mod, F, exec, firefox"
-      "$mod, T, exec, fuzzel"
+      # apps
+      "$mainMod, A, exec, alacritty"
+      "$mainMod, F, exec, firefox"
+      "$mainMod, T, exec, fuzzel"
+
+      # App management
+      "$mainMod SHIFT, Q, killactivate"
+      "$mainMod SHIFT, F, togglefloating"
+      "$mainMod SHIFT, M, exit"
+
+      # move focus with mainMod + hjkl
+      "$mainMod, h, movefocus, l"
+      "$mainMod, l, movefocus, r"
+      "$mainMod, k, movefocus, u"
+      "$mainMod, j, movefocus, d"
+      "$mainMod, Tab, cyclenext,"
+      "$mainMod, Tab, alterzorder,"
+
+      # Switch workspaces with mainMod + [0-9]
+      "$mainMod, 1, focusworkspaceoncurrentmonitor, 1"
+      "$mainMod, 2, focusworkspaceoncurrentmonitor, 2"
+      "$mainMod, 3, focusworkspaceoncurrentmonitor, 3"
+      "$mainMod, 4, focusworkspaceoncurrentmonitor, 4"
+      "$mainMod, 5, focusworkspaceoncurrentmonitor, 5"
+      "$mainMod, 6, focusworkspaceoncurrentmonitor, 6"
+      "$mainMod, 7, focusworkspaceoncurrentmonitor, 7"
+      "$mainMod, 8, focusworkspaceoncurrentmonitor, 8"
+      "$mainMod, 9, focusworkspaceoncurrentmonitor, 9"
+      "$mainMod, 0, focusworkspaceoncurrentmonitor, 10"
+
+      # Move active window to a workspace with mainMod + SHIFT + [0-9]
+      "$mainMod SHIFT, 1, movetoworkspace, 1"
+      "$mainMod SHIFT, 2, movetoworkspace, 2"
+      "$mainMod SHIFT, 3, movetoworkspace, 3"
+      "$mainMod SHIFT, 4, movetoworkspace, 4"
+      "$mainMod SHIFT, 5, movetoworkspace, 5"
+      "$mainMod SHIFT, 6, movetoworkspace, 6"
+      "$mainMod SHIFT, 7, movetoworkspace, 7"
+      "$mainMod SHIFT, 8, movetoworkspace, 8"
+      "$mainMod SHIFT, 9, movetoworkspace, 9"
+      "$mainMod SHIFT, 0, movetoworkspace, 10"
     ];
   };
 }
