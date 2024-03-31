6ea01d1d6c470da850a0c66c5bbe21e6a777a6ae
spsalmon <psalmonsacha@gmail.com>
2024-03-31 11:48:07 +0200

    few changes to hyprland

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -26,7 +26,8 @@
     input = {
       kb_layout = "fr";
       kb_options="grp:caps_toggle";
-      sensitivity = "0.5";
+      sensitivity = "-0.5";
+      accel_profile = "flat";
     };
 
     misc = {
@@ -73,28 +74,28 @@
       "$mainMod, Tab, alterzorder"
 
       # Switch workspaces with mainMod + [0-9]
-      "$mainMod, 1, workspace, 1"
-      "$mainMod, 2, workspace, 2"
-      "$mainMod, 3, workspace, 3"
-      "$mainMod, 4, workspace, 4"
-      "$mainMod, 5, workspace, 5"
-      "$mainMod, 6, workspace, 6"
-      "$mainMod, 7, workspace, 7"
-      "$mainMod, 8, workspace, 8"
-      "$mainMod, 9, workspace, 9"
-      "$mainMod, 0, workspace, 10"
+      "$mainMod, 1, workspace, &"
+      "$mainMod, 2, workspace, é"
+      "$mainMod, 3, workspace, ''\""
+      "$mainMod, 4, workspace, ''\'"
+      "$mainMod, 5, workspace, ("
+      "$mainMod, 6, workspace, -"
+      "$mainMod, 7, workspace, è"
+      "$mainMod, 8, workspace, _"
+      "$mainMod, 9, workspace, ç"
+      "$mainMod, 0, workspace, à"
 
       # Move active window to a workspace with mainMod + SHIFT + [0-9]
-      "$mainMod SHIFT, 1, movetoworkspace, 1"
-      "$mainMod SHIFT, 2, movetoworkspace, 2"
-      "$mainMod SHIFT, 3, movetoworkspace, 3"
-      "$mainMod SHIFT, 4, movetoworkspace, 4"
-      "$mainMod SHIFT, 5, movetoworkspace, 5"
-      "$mainMod SHIFT, 6, movetoworkspace, 6"
-      "$mainMod SHIFT, 7, movetoworkspace, 7"
-      "$mainMod SHIFT, 8, movetoworkspace, 8"
-      "$mainMod SHIFT, 9, movetoworkspace, 9"
-      "$mainMod SHIFT, 0, movetoworkspace, 10"
+      "$mainMod SHIFT, 1, movetoworkspace, &"
+      "$mainMod SHIFT, 2, movetoworkspace, é"
+      "$mainMod SHIFT, 3, movetoworkspace, ''\""
+      "$mainMod SHIFT, 4, movetoworkspace, ''\'"
+      "$mainMod SHIFT, 5, movetoworkspace, ("
+      "$mainMod SHIFT, 6, movetoworkspace, -"
+      "$mainMod SHIFT, 7, movetoworkspace, è"
+      "$mainMod SHIFT, 8, movetoworkspace, _"
+      "$mainMod SHIFT, 9, movetoworkspace, ç"
+      "$mainMod SHIFT, 0, movetoworkspace, à"
     ];
   };
 }
