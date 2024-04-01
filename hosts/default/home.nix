266b030120a631772d836d1c7626cc55e94afb62
spsalmon <psalmonsacha@gmail.com>
2024-04-01 21:59:13 +0200

    customizing waybar

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -4,7 +4,7 @@
   imports =
     [
       ../../modules/home-manager/hyprland.nix
-      #../../modules/home-manager/waybar.nix
+      ../../modules/home-manager/waybar.nix
     ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
