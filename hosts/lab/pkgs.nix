44fb194b65383c528bf31cd0167607c9511c9358
spsalmon <psalmonsacha@gmail.com>
2024-04-29 15:11:59 +0200

    removing sensitive info

--- hosts/lab/pkgs.nix
+++ hosts/lab/pkgs.nix
@@ -8,9 +8,4 @@
     ];
 
   boot.kernelPackages = pkgs.linuxPackages;
-
-  # Change MAC adress to access ethernet in the lab
-  #networking.interfaces.enp0s13f0u3u4c2.macAddress = "a0:29:19:a0:d8:68";
-  #networking.interfaces.wlp0s20f3.macAddress = "a0:29:19:a0:d8:68";
-  #networking.networkmanager.ethernet.macAddress = "a0:29:19:a0:d8:68";
 }
