f5ce8b2449acdde92674eafacabde3093b203d97
spsalmon <psalmonsacha@gmail.com>
2024-04-29 15:01:17 +0200

    trying something

--- hosts/lab/pkgs.nix
+++ hosts/lab/pkgs.nix
@@ -12,7 +12,5 @@
   # Change MAC adress to access ethernet in the lab
   #networking.interfaces.enp0s13f0u3u4c2.macAddress = "a0:29:19:a0:d8:68";
   #networking.interfaces.wlp0s20f3.macAddress = "a0:29:19:a0:d8:68";
-  systemd.network.wait-online.enable = false;
-  boot.initrd.systemd.network.wait-online.enable = false;
   networking.networkmanager.ethernet.macAddress = "a0:29:19:a0:d8:68";
 }
