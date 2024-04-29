e00f6f52548fbb396f7d355b5dde1d4142edaf60
spsalmon <psalmonsacha@gmail.com>
2024-04-29 15:04:07 +0200

    reverting changes

--- hosts/lab/pkgs.nix
+++ hosts/lab/pkgs.nix
@@ -12,5 +12,5 @@
   # Change MAC adress to access ethernet in the lab
   #networking.interfaces.enp0s13f0u3u4c2.macAddress = "a0:29:19:a0:d8:68";
   #networking.interfaces.wlp0s20f3.macAddress = "a0:29:19:a0:d8:68";
-  networking.networkmanager.ethernet.macAddress = "a0:29:19:a0:d8:68";
+  #networking.networkmanager.ethernet.macAddress = "a0:29:19:a0:d8:68";
 }
