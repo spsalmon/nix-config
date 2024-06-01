6da35a1b5c5a4e24beab6fd014c791481569fa4f
spsalmon <psalmonsacha@gmail.com>
2024-06-01 11:59:55 +0200

    added a fish module

---
+++ modules/nixos/fish.nix
@@ -0,0 +1,26 @@
+  
+{ lib, config, pkgs, ... }:
+{
+  # Activate fish
+  programs.fish.enable = true;
+
+  programs.bash = {
+  interactiveShellInit = ''
+    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+    then
+      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    fi
+  '';
+  };
+
+  environment.systemPackages = with pkgs; [
+    fishPlugins.done
+    fishPlugins.fzf-fish
+    fishPlugins.forgit
+    fishPlugins.hydro
+    fzf
+    fishPlugins.grc
+    grc
+  ];
+}
