2bfe94b351a4034e19702a393aa6aecb1b0e0967
spsalmon <psalmonsacha@gmail.com>
2024-07-25 16:39:05 +0200

    added some utilities

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -52,6 +52,9 @@
     pkgs.networkmanager
     pkgs.nmap
     pkgs.ncdu
+    pkgs.hyfetch
+    pkgs.mangohud
+    pkgs.lm_sensors
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
