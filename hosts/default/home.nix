cc1cf3a05fd268384546bd70ce3e924cc4916392
spsalmon <psalmonsacha@gmail.com>
2024-08-17 02:02:05 +0200

    trying to fix home manager

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -26,6 +26,8 @@
     userEmail = "psalmonsacha@gmail.com";
   };
 
+  home-manager.backupFileExtension = "backup";
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
