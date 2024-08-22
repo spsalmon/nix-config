15540dc01a439beda14631e2afee0a1331729715
spsalmon <sacha.psalmon@unibe.ch>
2024-08-22 12:40:00 +0200

    adding jupyter core

--- modules/nixos/python.nix
+++ modules/nixos/python.nix
@@ -1,6 +1,6 @@
 { lib, config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    (python312.withPackages(ps: with ps; [ numpy matplotlib python-lsp-server pyls-isort python-lsp-ruff python-lsp-black ipykernel]))
+    (python312.withPackages(ps: with ps; [ numpy matplotlib python-lsp-server pyls-isort python-lsp-ruff python-lsp-black ipykernel jupyter-core]))
   ];
 }
