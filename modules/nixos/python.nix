ed830b8ccf43b917356767ec1c20a25946d3a375
spsalmon <sacha.psalmon@unibe.ch>
2024-08-22 12:51:10 +0200

    added micromamba

--- modules/nixos/python.nix
+++ modules/nixos/python.nix
@@ -2,5 +2,6 @@
 {
   environment.systemPackages = with pkgs; [
     (python312.withPackages(ps: with ps; [ numpy matplotlib python-lsp-server pyls-isort python-lsp-ruff python-lsp-black ipykernel jupyter-core]))
+    micromamba
   ];
 }
