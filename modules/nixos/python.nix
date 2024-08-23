e774eafcfe2137f28e02f3aafba2d0593cce6ece
spsalmon <psalmonsacha@gmail.com>
2024-08-23 16:31:35 +0200

    trying to make micromamba work out of the box

--- modules/nixos/python.nix
+++ modules/nixos/python.nix
@@ -4,4 +4,14 @@
     (python312.withPackages(ps: with ps; [ numpy matplotlib python-lsp-server pyls-isort python-lsp-ruff python-lsp-black ipykernel jupyter-core]))
     micromamba
   ];
+
+  # Required to make mamba-managed Python run without an FHS environment
+  programs.nix-ld.enable = true;
+
+  programs.fish = {
+    interactiveShellInit = ''
+      mkdir -p $MAMBA_ROOT_PREFIX
+      eval "$(micromamba shell hook -s fish)"
+    '';
+  };
 }
