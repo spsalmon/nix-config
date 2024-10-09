863dc46c03be8b605739f041e88291b37844ac98
spsalmon <psalmonsacha@gmail.com>
2024-10-09 08:45:56 +0200

    added synths back

--- modules/nixos/music.nix
+++ modules/nixos/music.nix
@@ -7,17 +7,13 @@
     reaper
     ardour
 
-    # synths
-    vital
-    surge-XT
-
-    # drums
-    drumkv1
-    hydrogen
-
     # Support for Windows VST2/VST3 plugins
     yabridge
     yabridgectl
     wineWowPackages.stable
+
+    # synths
+    surge-xt
+    vital
   ];
 }
