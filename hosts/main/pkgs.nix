3749e80055b16f9a3ef5627f6d081d9e2f0024cb
spsalmon <psalmonsacha@gmail.com>
2025-01-31 21:36:03 +0100

    trying to fix virtualbox

--- hosts/main/pkgs.nix
+++ hosts/main/pkgs.nix
@@ -19,7 +19,7 @@
       ../../modules/nixos/emulators.nix
       ../../modules/nixos/krita.nix
       ../../modules/nixos/docker.nix
-      ../../modules/nixos/quickemu.nix
+      #../../modules/nixos/quickemu.nix
       ../../modules/nixos/virtualbox.nix
       ../../modules/nixos/vscode.nix
       ../../modules/nixos/secure_boot.nix
--- modules/nixos/virtualbox.nix
+++ modules/nixos/virtualbox.nix
@@ -3,4 +3,5 @@
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = ["${username}"];
   virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.host.enableKvm = true;
 }
