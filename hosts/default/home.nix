786c796ffab30b279d631f85a8cb07e9b6c458aa
spsalmon <psalmonsacha@gmail.com>
2025-04-29 07:51:43 +0200

    removed double option

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -35,8 +35,7 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
-
-  nixpkgs.config.allowUnfree = true;
+  
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
