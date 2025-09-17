096ea6baa6a3f09a16614cf4315f6994e8dc7af1
spsalmon <psalmonsacha@gmail.com>
2025-09-17 23:21:47 +0200

    updated flake

--- modules/nixos/ventoy.nix
+++ modules/nixos/ventoy.nix
@@ -1,7 +1,7 @@
 { lib, config, pkgs, ... }:
 {
   nixpkgs.config.permittedInsecurePackages = [
-    "ventoy-qt5-1.1.05"
+    "ventoy-qt5-1.1.07"
   ];
   environment.systemPackages = with pkgs; [
     ventoy-full-qt
