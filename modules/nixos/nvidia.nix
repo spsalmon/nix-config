c44ca2e0e4408867337e6b8cc78131bf6e9ca39a
spsalmon <psalmonsacha@gmail.com>
2026-02-25 09:24:51 +0100

    trying other driver

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -35,6 +35,8 @@
 	  # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
+    open = false;
+
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
