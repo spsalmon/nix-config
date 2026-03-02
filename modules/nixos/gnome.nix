d773460682a8b1b15ce057176d144aeb217c4aa7
spsalmon <psalmonsacha@gmail.com>
2026-03-02 22:41:20 +0100

    trying to go to stable so it stops breaking my balls

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -25,45 +25,18 @@
   environment.gnome.excludePackages = with pkgs; [
     orca
     evince
-    # file-roller
     geary
-    # seahorse
-    # sushi
-    # sysprof
-    #
-    # gnome-shell-extensions
-    #
-    # adwaita-icon-theme
-    # nixos-background-info
     gnome-backgrounds
-    # gnome-bluetooth
-    # gnome-color-manager
-    # gnome-control-center
-    # gnome-shell-extensions
-    gnome-tour # GNOME Shell detects the .desktop file on first log-in.
-    gnome-user-docs
-    # glib # for gsettings program
-    # gnome-menus
-    # gtk3.out # for gtk-launch program
-    # xdg-user-dirs # Update user dirs as described in https://freedesktop.org/wiki/Software/xdg-user-dirs/
-    # xdg-user-dirs-gtk # Used to create the default bookmarks
-    #
     baobab
     epiphany
     gnome-text-editor
     gnome-calendar
     gnome-characters
-    # gnome-clocks
     gnome-console
     gnome-contacts
-    gnome-font-viewer
-    gnome-logs
     gnome-maps
     gnome-music
-    # gnome-system-monitor
     gnome-weather
-    # loupe
-    # nautilus
     gnome-connections
     simple-scan
     snapshot
