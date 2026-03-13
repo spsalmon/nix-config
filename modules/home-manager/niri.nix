51fc362b697e3840ee2c1350f3eb7838862ed019
spsalmon <psalmonsacha@gmail.com>
2026-03-13 08:49:32 +0100

    typo time

--- modules/home-manager/niri.nix
+++ modules/home-manager/niri.nix
@@ -1,6 +1,6 @@
 {pkgs, config, lib, inputs, ... }:
 {
-  programs.niri.settings.gestures.hot-corners.enable = false
+  programs.niri.settings.gestures.hot-corners.enable = false;
   programs.niri.settings.binds = {
     "Mod+D".action.spawn = "fuzzel";
     "Mod+B".action.spawn = "firefox";
