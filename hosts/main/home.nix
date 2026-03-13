2f4cdc2adb725841ce1750ab370d8ab65dd90be6
spsalmon <psalmonsacha@gmail.com>
2026-03-13 08:48:11 +0100

    noctalia was not the source of the problem

--- hosts/main/home.nix
+++ hosts/main/home.nix
@@ -5,6 +5,6 @@
     [
       ../default/home.nix
       #../../modules/home-manager/openbox.nix
-      #../../modules/home-manager/noctalia_shell.nix
+      ../../modules/home-manager/noctalia_shell.nix
     ];
 }
