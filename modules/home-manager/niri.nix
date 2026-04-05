b1f0b7799e0b244bfcbb78f6fd5276f20af14d79
spsalmon <psalmonsacha@gmail.com>
2026-04-05 23:10:40 +0200

    trying to add new keybinds

--- modules/home-manager/niri.nix
+++ modules/home-manager/niri.nix
@@ -2,9 +2,15 @@
 {
   programs.niri.settings.gestures.hot-corners.enable = false;
   programs.niri.settings.binds = {
+
+    # launching and spawning
     "Mod+R".action.spawn = "fuzzel";
     "Mod+B".action.spawn = "firefox";
     "Mod+T".action.spawn = "kitty";
+
+    # navigation
+    "Mod+H".action.focus-column-left;
+    "Mod+L".action.focus-column-right;
     "Mod+1".action.focus-workspace = 1;
   };
 }
