6feed788b74c5a0868cc3ed359c11f99a4b7ead1
spsalmon <psalmonsacha@gmail.com>
2026-04-05 22:50:30 +0200

    switching to docker

--- modules/nixos/podman.nix
+++ modules/nixos/podman.nix
@@ -2,17 +2,16 @@
 {
   # Enable common container config files in /etc/containers
   virtualisation.containers.enable = true;
+
   virtualisation = {
-    podman = {
+    podman.enable = false;
+    docker = {
       enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
+      enableOnBoot = true;
     };
-  };
+  }
+
+  users.users.sacha.extraGroups = [ "docker" ];
 
   # Useful other development tools
   environment.systemPackages = with pkgs; [
