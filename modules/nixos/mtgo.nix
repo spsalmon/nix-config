5288bc1e16e09f491c41fbb60dafcfa3a34afcda
spsalmon <psalmonsacha@gmail.com>
2026-05-24 22:15:37 +0200

    trying to fix mtgo

--- modules/nixos/mtgo.nix
+++ modules/nixos/mtgo.nix
@@ -39,7 +39,7 @@ let
         winetricks -q corefonts calibri tahoma
         WINEDLLOVERRIDES="mscoree=" taskset -c 0 winetricks -q -f dotnet48
         winetricks -q vcrun2022 d3dcompiler_47
-        winetricks -q win10 sound=alsa renderer=gdi
+        winetricks -q win10 sound=disabled renderer=gdi
 
         echo "[mtgo] Fetching MTGO ClickOnce installer..."
         curl -fL -o "$WINEPREFIX/mtgo-setup.exe" \
