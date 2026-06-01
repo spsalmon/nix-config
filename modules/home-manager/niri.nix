d876527eef064b055f544c5bcfef00d0e4ae8da1
spsalmon <psalmonsacha@gmail.com>
2026-06-01 08:04:32 +0200

    keybinds should be better

--- modules/home-manager/niri.nix
+++ modules/home-manager/niri.nix
@@ -4,6 +4,19 @@
     { command = [ "noctalia-shell" ]; }
   ];
   programs.niri.settings.gestures.hot-corners.enable = false;
+
+  programs.niri.settings.workspaces = {
+    "1" = {};
+    "2" = {};
+    "3" = {};
+    "4" = {};
+    "5" = {};
+    "6" = {};
+    "7" = {};
+    "8" = {};
+    "9" = {};
+  };
+
   programs.niri.settings.binds = {
 
     # launching and spawning
@@ -21,19 +34,23 @@
     "Mod+K".action.focus-window-up = [];
 
     # focus workspace (by number)
-    "Mod+1".action.focus-workspace = 1;
-    "Mod+2".action.focus-workspace = 2;
-    "Mod+3".action.focus-workspace = 3;
-    "Mod+4".action.focus-workspace = 4;
-    "Mod+5".action.focus-workspace = 5;
-    "Mod+6".action.focus-workspace = 6;
-    "Mod+7".action.focus-workspace = 7;
-    "Mod+8".action.focus-workspace = 8;
-    "Mod+9".action.focus-workspace = 9;
+    "Mod+1".action.focus-workspace = "1";
+    "Mod+2".action.focus-workspace = "2";
+    "Mod+3".action.focus-workspace = "3";
+    "Mod+4".action.focus-workspace = "4";
+    "Mod+5".action.focus-workspace = "5";
+    "Mod+6".action.focus-workspace = "6";
+    "Mod+7".action.focus-workspace = "7";
+    "Mod+8".action.focus-workspace = "8";
+    "Mod+9".action.focus-workspace = "9";
 
     # focus workspace (relative)
     "Mod+Ctrl+J".action.focus-workspace-down = [];
     "Mod+Ctrl+K".action.focus-workspace-up = [];
+
+    # focus monitor
+    "Mod+Ctrl+H".action.focus-monitor-left = [];
+    "Mod+Ctrl+L".action.focus-monitor-right = [];
 
     # move windows/columns within workspace
     "Mod+Shift+H".action.move-column-left = [];
@@ -42,19 +59,23 @@
     "Mod+Shift+K".action.move-window-up = [];
 
     # move window to workspace (by number)
-    "Mod+Shift+1".action.move-window-to-workspace = 1;
-    "Mod+Shift+2".action.move-window-to-workspace = 2;
-    "Mod+Shift+3".action.move-window-to-workspace = 3;
-    "Mod+Shift+4".action.move-window-to-workspace = 4;
-    "Mod+Shift+5".action.move-window-to-workspace = 5;
-    "Mod+Shift+6".action.move-window-to-workspace = 6;
-    "Mod+Shift+7".action.move-window-to-workspace = 7;
-    "Mod+Shift+8".action.move-window-to-workspace = 8;
-    "Mod+Shift+9".action.move-window-to-workspace = 9;
+    "Mod+Shift+1".action.move-window-to-workspace = "1";
+    "Mod+Shift+2".action.move-window-to-workspace = "2";
+    "Mod+Shift+3".action.move-window-to-workspace = "3";
+    "Mod+Shift+4".action.move-window-to-workspace = "4";
+    "Mod+Shift+5".action.move-window-to-workspace = "5";
+    "Mod+Shift+6".action.move-window-to-workspace = "6";
+    "Mod+Shift+7".action.move-window-to-workspace = "7";
+    "Mod+Shift+8".action.move-window-to-workspace = "8";
+    "Mod+Shift+9".action.move-window-to-workspace = "9";
 
     # move window to workspace (relative)
     "Mod+Shift+Ctrl+J".action.move-window-to-workspace-down = [];
     "Mod+Shift+Ctrl+K".action.move-window-to-workspace-up = [];
+
+    # move column to monitor
+    "Mod+Shift+Ctrl+H".action.move-column-to-monitor-left = [];
+    "Mod+Shift+Ctrl+L".action.move-column-to-monitor-right = [];
 
     # window state
     "Mod+F".action.fullscreen-window = [];
